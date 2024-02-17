f859c71b1bf90c2a0d0d098ec0655292d91e5f8f
Amoolya-Reddy <amoolya.gali@gmail.com>
2024-02-17 12:00:13 -0800

    Updates

---
+++ precache-manifest.ee66902824c67800809e51d362867f10.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd5f33f13f0a04f0931f60e6dcee6a1",
+    "url": "/uniedge/index.html"
+  },
+  {
+    "revision": "3d472eef1e712731d5f7",
+    "url": "/uniedge/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "96fe959bf3bd6b5219b6",
+    "url": "/uniedge/static/css/main.a55ca8c8.chunk.css"
+  },
+  {
+    "revision": "3d472eef1e712731d5f7",
+    "url": "/uniedge/static/js/2.4772f445.chunk.js"
+  },
+  {
+    "revision": "96fe959bf3bd6b5219b6",
+    "url": "/uniedge/static/js/main.2dd29803.chunk.js"
+  },
+  {
+    "revision": "2d41b0ca683a8b6671dd",
+    "url": "/uniedge/static/js/runtime~main.ba893708.js"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/uniedge/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/uniedge/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/uniedge/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/uniedge/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/uniedge/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/uniedge/static/media/square1.ca257073.png"
+  }
+]);
