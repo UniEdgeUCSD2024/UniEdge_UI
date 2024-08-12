738d1d055cb4bdb9064e3e60e77b05df335183dc
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2024-08-12 12:02:26 +0530

    remove background from login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useRef, useState } from 'react';
 import {
   Form,
   Button,
@@ -13,15 +13,15 @@ import {
   CardBody,
   CardTitle,
   CardFooter,
-} from "reactstrap";
-import { useAuth, AuthContext } from "../context/AuthContext";
-import { Link, useNavigate } from "react-router-dom";
+} from 'reactstrap';
+import { useAuth, AuthContext } from '../context/AuthContext';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const { login } = useAuth();
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
   const history = useNavigate();
   const { userKeys } = useContext(AuthContext);
@@ -29,102 +29,97 @@ export default function Login() {
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      setError("");
+      setError('');
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
       const token = localStorage.getItem('token');
       if (userKeys && userKeys.role) {
         if (userKeys.role === 'Student') {
-          history("/student");
+          history('/student');
         } else {
-          history("/RecruiterHomePage");
+          history('/RecruiterHomePage');
         }
-        const response = await fetch('https://uniedge-functions.azurewebsites.net/checkuser', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}` 
-          },
-          body: JSON.stringify({
-            email: userKeys.email,
-            role: userKeys.role,
-          }),
-        });      
-        const login_state = await response.json(); 
+        const response = await fetch(
+          'https://uniedge-functions.azurewebsites.net/checkuser',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              email: userKeys.email,
+              role: userKeys.role,
+            }),
+          }
+        );
+        const login_state = await response.json();
         if (login_state) {
-          updateLoginState(login_state)
+          updateLoginState(login_state);
         }
       } else {
-        setError("User keys not found or invalid");
+        setError('User keys not found or invalid');
       }
     } catch (error) {
       console.error(error);
-      setError("Failed to log in");
+      setError('Failed to log in');
     }
     setLoading(false);
-  }  
+  }
 
   function updateLoginState(loginState) {
     window.localStorage.setItem('login_state', JSON.stringify(loginState));
     window.dispatchEvent(new Event('loginStateUpdated'));
-}
+  }
 
   return (
     <>
-      <div className="wrapper">
-        <div className="page-header">
-          <div className="content">
+      <div className='wrapper'>
+        <div className='page-header'>
+          <div className='content'>
             <Container>
               <Row>
-                <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
-                  <Card className="card-register">
-                    <CardHeader>
-                      <CardImg
-                        alt="..."
-                        src={require("../assets/img/square-gold.png")}
-                      />
-                      <CardTitle tag="h3">Login</CardTitle>
-                    </CardHeader>
-                    <CardBody>
-                      {error && (
-                        <Alert
-                          variant="danger"
-                          style={{ backgroundColor: "red" }}
-                        >
-                          {error}
-                        </Alert>
-                      )}
-                      <Form className="form">
-                        <Input
-                          innerRef={emailRef}
-                          placeholder="Email"
-                          type="text"
-                        />
-                        <Input
-                          style={{ marginTop: "1rem" }}
-                          innerRef={passwordRef}
-                          placeholder="Password"
-                          type="password"
-                        />
-                      </Form>
-                    </CardBody>
-                    <CardFooter>
-                      <Button
-                        disabled={loading}
-                        className="btn-round"
-                        color="info"
-                        size="lg"
-                        onClick={handleSubmit}
-                      >
-                        Login
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                  <div className="text-center">
-                    <Link to="/forgotpassword">Forgot Password?</Link>
+                <Col className='offset-lg-0 offset-md-3' lg='5' md='6'>
+                  <h1 className='text-black font-weight-bold text-start'>
+                    Login
+                  </h1>
+
+                  {error && (
+                    <Alert variant='danger' style={{ backgroundColor: 'red' }}>
+                      {error}
+                    </Alert>
+                  )}
+                  <Form className='form'>
+                    <Input
+                      innerRef={emailRef}
+                      placeholder='Email'
+                      type='text'
+                    />
+                    <Input
+                      style={{ marginTop: '1rem' }}
+                      innerRef={passwordRef}
+                      placeholder='Password'
+                      type='password'
+                    />
+                  </Form>
+
+                  <div className='d-flex'>
+                    <Button
+                      disabled={loading}
+                      className='btn-round'
+                      color='info'
+                      size='lg'
+                      onClick={handleSubmit}
+                    >
+                      Login
+                    </Button>
                   </div>
-                  <div className="w-100 text-center mt-2">
-                    Need an account? <Link to="/register">Sign Up</Link>
+
+                  <div className='text-start'>
+                    <Link to='/forgotpassword'>Forgot Password?</Link>
+                  </div>
+                  <div className='w-100 text-center mt-2'>
+                    Need an account? <Link to='/register'>Sign Up</Link>
                   </div>
                 </Col>
                 <Col>
